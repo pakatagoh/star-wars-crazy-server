e93649c74166f75275b041ccac331c9b9d7b9ac6
Pakata Goh <pakatagohlh@gmail.com>
2019-03-24 17:42:06 +0800

    configure CORS and morgan

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 const movies = require('./routes/movies');
 
+const isDev = process.env.NODE_ENV !== 'production';
+
+const whitelist = ['https://api-starwarscrazy.herokuapp.com'];
+
+isDev && whitelist.push('http://localhost:3000');
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+1;
+app.use(morgan(isDev ? 'dev' : 'tiny'));
 app.use(express.json());
-app.use('/api/v1/movies', movies);
+app.use('/v1/movies', movies);
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello world');
