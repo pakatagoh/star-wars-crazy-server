40cb1593528c16eb29ea834dde6926e88d69bf9e
Pakata Goh <pakatagohlh@gmail.com>
2019-03-24 15:17:23 +0800

    Install dotenv. initial hello world server

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('./app');
+
+const PORT = process.env.PORT || 8080;
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+app.listen(PORT, () => {
+  if (isDev) {
+    // eslint-disable-next-line no-console
+    return console.log(`Dev server is listening on port ${PORT}`);
+  }
+  // eslint-disable-next-line no-console
+  return console.log(`Server is listening on port ${PORT}`);
+});
